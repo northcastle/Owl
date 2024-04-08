cfc492824048f6fc172dca4a9156dabc2ac10db5
northcastle <975887168@qq.com>
2024-04-08 23:14:51 +0800

    add some keyitems

--- src/components/owlkeyboard/OwlKeyBoard.vue
+++ src/components/owlkeyboard/OwlKeyBoard.vue
@@ -4,11 +4,88 @@
  -->
 <template>
     <div class="key-board-panel">
+        <!-- F数字行 -->
+        <OwlKeyBoardItem  key-value="ESC" :width-value="75"/>
+        <OwlKeyBoardItem  key-value="F1"/>
+        <OwlKeyBoardItem  key-value="F2"/>
+        <OwlKeyBoardItem  key-value="F3"/>
+        <OwlKeyBoardItem  key-value="F4"/>
+        <OwlKeyBoardItem  key-value="F5"/>
+        <OwlKeyBoardItem  key-value="F6"/>
+        <OwlKeyBoardItem  key-value="F7"/>
+        <OwlKeyBoardItem  key-value="F8"/>
+        <OwlKeyBoardItem  key-value="F9"/>
+        <OwlKeyBoardItem  key-value="F10"/>
+        <OwlKeyBoardItem  key-value="F11"/>
+        <OwlKeyBoardItem  key-value="F12"/>
+        <OwlKeyBoardItem  key-value="🥇"/>
+        <br>
+        <!-- 数字行 -->
+        <OwlKeyBoardItem  key-value="`~"/>
+        <OwlKeyBoardItem  key-value="1"/>
+        <OwlKeyBoardItem  key-value="2"/>
+        <OwlKeyBoardItem  key-value="3"/>
+        <OwlKeyBoardItem  key-value="4"/>
+        <OwlKeyBoardItem  key-value="5"/>
+        <OwlKeyBoardItem  key-value="6"/>
+        <OwlKeyBoardItem  key-value="7"/>
+        <OwlKeyBoardItem  key-value="8"/>
+        <OwlKeyBoardItem  key-value="9"/>
+        <OwlKeyBoardItem  key-value="0"/>
+        <OwlKeyBoardItem  key-value="-"/>
+        <OwlKeyBoardItem  key-value="+"/>
+        <OwlKeyBoardItem  key-value="del" :width-value="75"/>
+        <br>
+        <!-- 第一行 -->
+        <OwlKeyBoardItem  key-value="Tab" :width-value="75"/>
         <OwlKeyBoardItem  key-value="Q"/>
         <OwlKeyBoardItem  key-value="W"/>
         <OwlKeyBoardItem  key-value="E"/>
         <OwlKeyBoardItem  key-value="R"/>
-        <OwlKeyBoardItem  key-value="T" :width-value="100" />
+        <OwlKeyBoardItem  key-value="T"/>
+        <OwlKeyBoardItem  key-value="Y"/>
+        <OwlKeyBoardItem  key-value="U"/>
+        <OwlKeyBoardItem  key-value="I"/>
+        <OwlKeyBoardItem  key-value="O"/>
+        <OwlKeyBoardItem  key-value="P"/>
+        <OwlKeyBoardItem  key-value="["/>
+        <OwlKeyBoardItem  key-value="]"/>
+        <OwlKeyBoardItem  key-value="\"/>
+        <br>
+
+        <!-- 第二行 -->
+        <OwlKeyBoardItem  key-value="Cap" :width-value="95"/>
+        <OwlKeyBoardItem  key-value="A"/>
+        <OwlKeyBoardItem  key-value="S"/>
+        <OwlKeyBoardItem  key-value="D"/>
+        <OwlKeyBoardItem  key-value="F"/>
+        <OwlKeyBoardItem  key-value="G"/>
+        <OwlKeyBoardItem  key-value="H"/>
+        <OwlKeyBoardItem  key-value="J"/>
+        <OwlKeyBoardItem  key-value="K"/>
+        <OwlKeyBoardItem  key-value="L"/>
+        <OwlKeyBoardItem  key-value=": ;"/>
+        <OwlKeyBoardItem  key-value="” ’"/>
+        <OwlKeyBoardItem  key-value="Enter" :width-value="95"/>
+        <br>
+        
+        <!-- 第三行 -->
+        <OwlKeyBoardItem  key-value="Shift" :width-value="130"/>
+        <OwlKeyBoardItem  key-value="Z"/>
+        <OwlKeyBoardItem  key-value="X"/>
+        <OwlKeyBoardItem  key-value="C"/>
+        <OwlKeyBoardItem  key-value="V"/>
+        <OwlKeyBoardItem  key-value="B"/>
+        <OwlKeyBoardItem  key-value="N"/>
+        <OwlKeyBoardItem  key-value="M"/>
+        <OwlKeyBoardItem  key-value="，"/>
+        <OwlKeyBoardItem  key-value="。."/>
+        <OwlKeyBoardItem  key-value="/ ?"/>
+        <OwlKeyBoardItem  key-value="Shift" :width-value="130"/>
+      
+
+
+        <!-- <OwlKeyBoardItem  key-value="T" :width-value="100" /> -->
     </div>
 </template>
 
--- src/components/owlkeyboard/OwlKeyBoardItem.vue
+++ src/components/owlkeyboard/OwlKeyBoardItem.vue
@@ -26,15 +26,62 @@
    // 键盘按键 按下的标识
    const keyItemPressFlag:Ref<boolean> = ref(false)
 
+
+    // 键盘按键的基本数据对象
+    interface KeyItem{
+        keyCode:number,
+        keyValue:string | number
+    }
+
+    let keyItemList:KeyItem[] = [
+
+        
+        {keyCode:48,keyValue :0},{keyCode:49,keyValue :1},{keyCode:50,keyValue : 2},{keyCode:51,keyValue :3},{keyCode:52,keyValue :4},
+        {keyCode:53,keyValue :5},{keyCode:54,keyValue :6},{keyCode:55,keyValue :7},{keyCode:56,keyValue :8},{keyCode:57,keyValue :9},
+        
+
+        {keyCode:65,keyValue :'A'},{keyCode:66,keyValue :'B'},{keyCode:67,keyValue :'C'},{keyCode:68,keyValue :'D'},{keyCode:69,keyValue :'E'},
+        {keyCode:70,keyValue :'F'},{keyCode:71,keyValue :'G'},{keyCode:72,keyValue :'H'},{keyCode:73,keyValue :'I'},{keyCode:74,keyValue :'J'},
+        {keyCode:75,keyValue :'K'},{keyCode:76,keyValue :'L'},{keyCode:77,keyValue :'M'},{keyCode:78,keyValue :'N'},{keyCode:79,keyValue :'O'},
+        {keyCode:80,keyValue :'P'},{keyCode:81,keyValue :'Q'},{keyCode:82,keyValue :'R'},{keyCode:83,keyValue :'S'},{keyCode:84,keyValue :'T'},
+        {keyCode:85,keyValue :'U'},{keyCode:86,keyValue :'V'},{keyCode:87,keyValue :'W'},{keyCode:88,keyValue :'X'},{keyCode:89,keyValue :'Y'},
+        {keyCode:90,keyValue :'Z'},
+
+        
+
+        {keyCode:112,keyValue :'F1'},{keyCode:113,keyValue :'F2'},{keyCode:114,keyValue :'F3'},{keyCode:115,keyValue :'F4'},{keyCode:116,keyValue :'F5'},
+        {keyCode:117,keyValue :'F6'},{keyCode:118,keyValue :'F7'},{keyCode:119,keyValue :'F8'},{keyCode:120,keyValue :'F9'},{keyCode:121,keyValue :'F10'},
+        {keyCode:122,keyValue :'F11'},{keyCode:123,keyValue :'F12'},
+
+        {keyCode:27,keyValue :'ESC'},{keyCode:192,keyValue :'`~'},{keyCode:189,keyValue :'-'},{keyCode:187,keyValue :'+'},{keyCode:8,keyValue :'del'},
+        {keyCode:9,keyValue :'Tab'},{keyCode:219,keyValue :'['},{keyCode:221,keyValue :']'},{keyCode:220,keyValue :'\\'},{keyCode:20,keyValue :'Cap'},
+        {keyCode:186,keyValue :': ;'},{keyCode:222,keyValue :'” ’'},{keyCode:13,keyValue :'Enter'},{keyCode:16,keyValue :'Shift'},
+        {keyCode:188,keyValue :'，'},{keyCode:190,keyValue :'。.'},{keyCode:191,keyValue :'/ ?'},
+    
+    ]
+
     /**
      * 监听 按键按下的事件
      * @param event 
      */
    const handleKeyDown = (event:any)=>{
+
     console.log(event.keyCode,props.keyValue)
-    if(event.keyCode == 81 && props.keyValue == 'Q'){
-        keyItemPressFlag.value = true
+
+    // 判断键盘的值，对应的点亮某个按键
+    for(let i = 0;i < keyItemList.length;i++){
+        let item = keyItemList[i];
+        if(item.keyCode == event.keyCode && item.keyValue == props.keyValue){
+            keyItemPressFlag.value = true
+            break;
+        }
     }
+
+    // if(event.keyCode == 81 && props.keyValue == 'Q'){
+    //     keyItemPressFlag.value = true
+    // }else if(event.keyCode == 87 && props.keyValue == 'W'){
+    //     keyItemPressFlag.value = true
+    // }
    }
 
    /**
@@ -76,6 +123,7 @@
         border-radius: 10px;
         display: inline-block;
         margin-left: 16px;
+        margin-top: 16px;
         box-shadow: 0px 0px 5px 2px rgb(239, 219, 202);
 
     }
