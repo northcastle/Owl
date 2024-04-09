d369c30c4ec6f1fd7747c3918acd6c41cb87fb72
wanghongchang <975887168@qq.com>
2024-04-09 15:55:17 +0800

    keyboard success

--- src/components/owlkeyboard/OwlKeyBoard.vue
+++ src/components/owlkeyboard/OwlKeyBoard.vue
@@ -5,7 +5,7 @@
 <template>
     <div class="key-board-panel">
         <!-- F数字行 -->
-        <OwlKeyBoardItem  key-value="ESC" :width-value="75"/>
+        <OwlKeyBoardItem  key-value="Esc" :width-value="75"/>
         <OwlKeyBoardItem  key-value="F1"/>
         <OwlKeyBoardItem  key-value="F2"/>
         <OwlKeyBoardItem  key-value="F3"/>
@@ -22,18 +22,18 @@
         <br>
         <!-- 数字行 -->
         <OwlKeyBoardItem  key-value="`~"/>
-        <OwlKeyBoardItem  key-value="1"/>
-        <OwlKeyBoardItem  key-value="2"/>
-        <OwlKeyBoardItem  key-value="3"/>
-        <OwlKeyBoardItem  key-value="4"/>
-        <OwlKeyBoardItem  key-value="5"/>
-        <OwlKeyBoardItem  key-value="6"/>
-        <OwlKeyBoardItem  key-value="7"/>
-        <OwlKeyBoardItem  key-value="8"/>
-        <OwlKeyBoardItem  key-value="9"/>
-        <OwlKeyBoardItem  key-value="0"/>
-        <OwlKeyBoardItem  key-value="-"/>
-        <OwlKeyBoardItem  key-value="+"/>
+        <OwlKeyBoardItem  key-value="1 !"/>
+        <OwlKeyBoardItem  key-value="2 @"/>
+        <OwlKeyBoardItem  key-value="3 #"/>
+        <OwlKeyBoardItem  key-value="4 $"/>
+        <OwlKeyBoardItem  key-value="5 %"/>
+        <OwlKeyBoardItem  key-value="6 ^"/>
+        <OwlKeyBoardItem  key-value="7 &"/>
+        <OwlKeyBoardItem  key-value="8 *"/>
+        <OwlKeyBoardItem  key-value="9 ("/>
+        <OwlKeyBoardItem  key-value="0 )"/>
+        <OwlKeyBoardItem  key-value="- _"/>
+        <OwlKeyBoardItem  key-value="= +"/>
         <OwlKeyBoardItem  key-value="del" :width-value="75"/>
         <br>
         <!-- 第一行 -->
@@ -48,13 +48,13 @@
         <OwlKeyBoardItem  key-value="I"/>
         <OwlKeyBoardItem  key-value="O"/>
         <OwlKeyBoardItem  key-value="P"/>
-        <OwlKeyBoardItem  key-value="["/>
-        <OwlKeyBoardItem  key-value="]"/>
-        <OwlKeyBoardItem  key-value="\"/>
+        <OwlKeyBoardItem  key-value="[ {"/>
+        <OwlKeyBoardItem  key-value="] }"/>
+        <OwlKeyBoardItem  key-value="\ |"/>
         <br>
 
         <!-- 第二行 -->
-        <OwlKeyBoardItem  key-value="Cap" :width-value="95"/>
+        <OwlKeyBoardItem  key-value="Caps" :width-value="95"/>
         <OwlKeyBoardItem  key-value="A"/>
         <OwlKeyBoardItem  key-value="S"/>
         <OwlKeyBoardItem  key-value="D"/>
@@ -64,13 +64,13 @@
         <OwlKeyBoardItem  key-value="J"/>
         <OwlKeyBoardItem  key-value="K"/>
         <OwlKeyBoardItem  key-value="L"/>
-        <OwlKeyBoardItem  key-value=": ;"/>
-        <OwlKeyBoardItem  key-value="” ’"/>
-        <OwlKeyBoardItem  key-value="Enter" :width-value="95"/>
+        <OwlKeyBoardItem  key-value="; :"/>
+        <OwlKeyBoardItem  key-value="’ ”"/>
+        <OwlKeyBoardItem  key-value="Enter" :width-value="73"/>
         <br>
         
         <!-- 第三行 -->
-        <OwlKeyBoardItem  key-value="Shift" :width-value="130"/>
+        <OwlKeyBoardItem  key-value="Shift" :width-value="113"/>
         <OwlKeyBoardItem  key-value="Z"/>
         <OwlKeyBoardItem  key-value="X"/>
         <OwlKeyBoardItem  key-value="C"/>
@@ -78,14 +78,28 @@
         <OwlKeyBoardItem  key-value="B"/>
         <OwlKeyBoardItem  key-value="N"/>
         <OwlKeyBoardItem  key-value="M"/>
-        <OwlKeyBoardItem  key-value="，"/>
-        <OwlKeyBoardItem  key-value="。."/>
+        <OwlKeyBoardItem  key-value=", <"/>
+        <OwlKeyBoardItem  key-value=". >"/>
         <OwlKeyBoardItem  key-value="/ ?"/>
-        <OwlKeyBoardItem  key-value="Shift" :width-value="130"/>
+        <OwlKeyBoardItem  key-value="↑"  />
+        <OwlKeyBoardItem  key-value="Shift" :width-value="55"/>
+        <br>
+
+        <!-- 第四行 -->
+        <OwlKeyBoardItem  key-value="Fn"/>
+        <OwlKeyBoardItem  key-value="Ctrl"/>
+        <OwlKeyBoardItem  key-value="Alt"/>
+        <OwlKeyBoardItem  key-value="Win"/>
+        <OwlKeyBoardItem  key-value="Space" :width-value="260"/>
+        <OwlKeyBoardItem  key-value="Com"/>
+        <OwlKeyBoardItem  key-value="Opt" />
+        <OwlKeyBoardItem  key-value="←" :width-value="43"/>
+
+        
+        <OwlKeyBoardItem  key-value="↓"  />
+        
+        <OwlKeyBoardItem  key-value="→" :width-value="55"/>
       
-
-
-        <!-- <OwlKeyBoardItem  key-value="T" :width-value="100" /> -->
     </div>
 </template>
 
@@ -101,10 +115,13 @@ import OwlKeyBoardItem from './OwlKeyBoardItem.vue';
 
     .key-board-panel{
         border: 0px solid rgb(18, 216, 71);
-        height: 400px;
-        width: 1200px;
-        padding: 10px;
+        height: 310px;
+        width: 750px;
+        min-width: 750px;
+        min-height: 310px;
+        padding: 0px;
         background-color: white;
+        border-radius: 10px;
     }
 
 
--- src/components/owlkeyboard/OwlKeyBoardItem.vue
+++ src/components/owlkeyboard/OwlKeyBoardItem.vue
@@ -4,7 +4,7 @@
  -->
 <template>
     <div class="key-board-item" 
-        :style="{width: widthValue + 'px'}"
+        :style="{width: widthValue + 'px',height:heightValue+'px',lineHeight:heightValue+'px'}"
         :class="{keyItemPress:keyItemPressFlag}">
         {{ keyValue }}
     </div>
@@ -19,6 +19,7 @@
     interface KeyItemProps{
         keyValue?:string, // 键盘按键的值
         widthValue?:number, // 键盘按键的宽度，默认是 50px
+        heightValue?:number, // 键盘按键的高度，默认是 50px
         
     }
    const props =  defineProps<KeyItemProps>()
@@ -36,8 +37,20 @@
     let keyItemList:KeyItem[] = [
 
         
-        {keyCode:48,keyValue :0},{keyCode:49,keyValue :1},{keyCode:50,keyValue : 2},{keyCode:51,keyValue :3},{keyCode:52,keyValue :4},
-        {keyCode:53,keyValue :5},{keyCode:54,keyValue :6},{keyCode:55,keyValue :7},{keyCode:56,keyValue :8},{keyCode:57,keyValue :9},
+        {keyCode:192,keyValue :'`~'},
+        {keyCode:49,keyValue :'1 !'},
+        {keyCode:50,keyValue :'2 @'},
+        {keyCode:51,keyValue :'3 #'},
+        {keyCode:52,keyValue :'4 $'},
+        {keyCode:53,keyValue :'5 %'},
+        {keyCode:54,keyValue :'6 ^'},
+        {keyCode:55,keyValue :'7 &'},
+        {keyCode:56,keyValue :'8 *'},
+        {keyCode:57,keyValue :'9 ('},
+        {keyCode:48,keyValue :'0 )'},
+        {keyCode:189,keyValue :'- _'},
+        {keyCode:187,keyValue :'= +'},
+        {keyCode:8,keyValue :'del'},
         
 
         {keyCode:65,keyValue :'A'},{keyCode:66,keyValue :'B'},{keyCode:67,keyValue :'C'},{keyCode:68,keyValue :'D'},{keyCode:69,keyValue :'E'},
@@ -47,16 +60,48 @@
         {keyCode:85,keyValue :'U'},{keyCode:86,keyValue :'V'},{keyCode:87,keyValue :'W'},{keyCode:88,keyValue :'X'},{keyCode:89,keyValue :'Y'},
         {keyCode:90,keyValue :'Z'},
 
+        {keyCode:27,keyValue :'Esc'},
+        {keyCode:112,keyValue :'F1'},
+        {keyCode:113,keyValue :'F2'},
+        {keyCode:114,keyValue :'F3'},
+        {keyCode:115,keyValue :'F4'},
+        {keyCode:116,keyValue :'F5'},
+        {keyCode:117,keyValue :'F6'},
+        {keyCode:118,keyValue :'F7'},
+        {keyCode:119,keyValue :'F8'},
+        {keyCode:120,keyValue :'F9'},
+        {keyCode:121,keyValue :'F10'},
+        {keyCode:122,keyValue :'F11'},
+        {keyCode:123,keyValue :'F12'},
+
+       
         
+       
+        {keyCode:9,keyValue :'Tab'},
+        {keyCode:13,keyValue :'Enter'},
+        {keyCode:16,keyValue :'Shift'},
+        {keyCode:20,keyValue :'Caps'},
+        {keyCode:17,keyValue :'Ctrl'},
+        {keyCode:1800000,keyValue :'Alt'},
+        {keyCode:1800000,keyValue :'Opt'},
+        {keyCode:9100000,keyValue :'Win'},
+        {keyCode:9100000,keyValue :'Com'},
+        {keyCode:32,keyValue :'Space'},
 
-        {keyCode:112,keyValue :'F1'},{keyCode:113,keyValue :'F2'},{keyCode:114,keyValue :'F3'},{keyCode:115,keyValue :'F4'},{keyCode:116,keyValue :'F5'},
-        {keyCode:117,keyValue :'F6'},{keyCode:118,keyValue :'F7'},{keyCode:119,keyValue :'F8'},{keyCode:120,keyValue :'F9'},{keyCode:121,keyValue :'F10'},
-        {keyCode:122,keyValue :'F11'},{keyCode:123,keyValue :'F12'},
-
-        {keyCode:27,keyValue :'ESC'},{keyCode:192,keyValue :'`~'},{keyCode:189,keyValue :'-'},{keyCode:187,keyValue :'+'},{keyCode:8,keyValue :'del'},
-        {keyCode:9,keyValue :'Tab'},{keyCode:219,keyValue :'['},{keyCode:221,keyValue :']'},{keyCode:220,keyValue :'\\'},{keyCode:20,keyValue :'Cap'},
-        {keyCode:186,keyValue :': ;'},{keyCode:222,keyValue :'” ’'},{keyCode:13,keyValue :'Enter'},{keyCode:16,keyValue :'Shift'},
-        {keyCode:188,keyValue :'，'},{keyCode:190,keyValue :'。.'},{keyCode:191,keyValue :'/ ?'},
+        {keyCode:219,keyValue :'[ {'},
+        {keyCode:221,keyValue :'] }'},
+        {keyCode:220,keyValue :'\\ |'},
+        {keyCode:186,keyValue :'; :'},
+        {keyCode:222,keyValue :'’ ”'},
+        {keyCode:188,keyValue :', <'},
+        {keyCode:190,keyValue :'. >'},
+        {keyCode:191,keyValue :'/ ?'},
+        
+        
+        {keyCode:37,keyValue :'←'},
+        {keyCode:38,keyValue :'↑'},
+        {keyCode:39,keyValue :'→'},
+        {keyCode:40,keyValue :'↓'},
     
     ]
 
@@ -66,7 +111,7 @@
      */
    const handleKeyDown = (event:any)=>{
 
-    console.log(event.keyCode,props.keyValue)
+    // console.log(event.keyCode,props.keyValue)
 
     // 判断键盘的值，对应的点亮某个按键
     for(let i = 0;i < keyItemList.length;i++){
@@ -113,23 +158,26 @@
 
     .key-board-item{
         border: 1px solid rgb(11, 10, 10);
-        width: 50px;
-        height: 50px;
+        width: 40px;
+        height: 40px;
         font-weight: bold;
-        font-size: 18px;
+        font-size: 14px;
         text-align: center;
-        line-height: 50px;
+        line-height: 40px;
         color: black;
-        border-radius: 10px;
+        border-radius: 8px;
         display: inline-block;
-        margin-left: 16px;
-        margin-top: 16px;
-        box-shadow: 0px 0px 5px 2px rgb(239, 219, 202);
+        margin-left: 10px;
+        margin-top: 10px;
+        box-shadow: 0px 0px 1px 2px rgb(242 233 226)
 
     }
 
     .keyItemPress{
-        background-color: aqua;
+        color: white;
         box-shadow: 0px 0px 10px 0px rgb(6, 191, 197);
-    }
+        background-image: radial-gradient(circle, #b878ea, #7d9eff, #13bcff, #00d4ff, #00e8ff, #00f1f6, #00f8e7, #00ffd3, #40ffb1, #70ff88, #9eff58, #ccfc00);
+        /* background-image: radial-gradient(circle, #d16ba5, #c777b9, #ba83ca, #aa8fd8, #9a9ae1, #8aa7ec, #79b3f4, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1); */
+       
+       }
 </style>
