99c3918fdf0dcea94441784a76d6bbf0d3943184
northcastle <975887168@qq.com>
2024-04-11 22:38:25 +0800

    menu almostly success

--- src/components/owlMenuPanel/OwlMenuPanel.vue
+++ src/components/owlMenuPanel/OwlMenuPanel.vue
@@ -1,33 +1,52 @@
 
-<!-- 菜单栏组件 -->
+<!-- 菜单栏组件  -->
 <template>
-     <el-menu background-color="rgba(0,0,0,0)"  text-color="#fff" default-active="2">
-        <el-menu-item index="1">
-          <template #title>
-            <el-icon><location /></el-icon>
-            <span>Navigator One</span>
+    <el-scrollbar max-height="200px" >
+      <el-menu   background-color="rgba(0,0,0,0)" text-color="#fff" default-active="2" :router="true" >
+          <el-menu-item index="2">
+            <el-icon><Menu /></el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+         
+
+          <template v-for="menuItem in menuList">
+              <el-menu-item :index="menuItem.routerPath">
+                <el-icon><component :is="menuItem.icon"></component></el-icon>
+                
+                <span>{{ menuItem.text }}</span>
+          </el-menu-item>
           </template>
-        </el-menu-item>
-        <el-menu-item index="2">
-          <!-- <el-icon><icon-menu /></el-icon> -->
-          <el-icon><Menu /></el-icon>
-          <!-- <el-icon><MessageBox /></el-icon> -->
-          <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <el-icon><document /></el-icon>
-          <span>Navigator Three</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon><setting /></el-icon>
-          <span>Navigator Four</span>
-        </el-menu-item>
-      </el-menu>
+
+        </el-menu>
+    </el-scrollbar>
 </template>
 
 <script setup lang="ts">
 
+import {reactive} from 'vue'
+
+// 导入菜单对象的类型
+import type { MenuObj } from './OwlMenuPanelType';
+
+// 声明一些菜单元素
+const menuList:Array<MenuObj> = reactive([
+  {id:'a',text:'菜单1',icon:'Menu',routerPath:'/owlkeyboard'}
+])
+
+
+
 </script>
 
 <style scoped>
+
+/* 重写滚动条原来的颜色 */
+.el-scrollbar {
+    --el-scrollbar-bg-color: #959791;
+}
+
+/* 重写菜单原来的右侧边框 */
+.el-menu{
+  border-right: 0px solid red;
+}
+
 </style>
---
+++ src/components/owlMenuPanel/OwlMenuPanelType.ts
@@ -0,0 +1,10 @@
+
+/**
+ * 菜单对象
+ */
+export interface MenuObj{
+    id:string, // 菜单ID
+    text:string, // 菜单名称
+    icon?:string, // 菜单图标
+    routerPath?:string, // 菜单路由
+}
