fa290938004131159405eb97c97438e6ffd153b0
northcastle <975887168@qq.com>
2024-04-14 16:39:31 +0800

    finish the accuracy view

--- src/views/typePractice/TypingPractice.vue
+++ src/views/typePractice/TypingPractice.vue
@@ -7,11 +7,20 @@
         <el-row>
             <!-- 顶部的事件和准确率 -->
             <el-col :span="12">
-                <div style="height: 70px;">
+                <div style="height: 70px;margin-right: 5px;">
                     <OwlTimer />
                 </div> 
             </el-col>
-            <el-col :span="12"><div style="height: 70px;">准确率</div></el-col>
+            <el-col :span="12">
+                <div style="height: 70px;margin-left: 5px;">
+                    <div class="accuracy-container">
+                        <el-icon size="1.5rem" color="black" style="position: absolute;margin-top: 4px;"><Medal /></el-icon>
+                        <span class="accuracy-text">
+                            Accuracy (准确率)：{{ accuracyValue }}
+                        </span>
+                    </div>
+                </div>
+            </el-col>
             <!-- 左侧的案例内容 -->
             <el-col :span="12">
                 <div class="text-box-left">
@@ -53,6 +62,9 @@ import {typingCaseList} from './TypingCaseData'
 // 输入的文本
 const typingText = ref('')
 
+// 准确率
+const accuracyValue = ref('--%')
+
 </script>
 
 
@@ -64,6 +76,25 @@ const typingText = ref('')
     /* height: 400px; */
     /* width: 1200px; */
    
+}
+
+.accuracy-container{
+    border: 0px solid rgb(0, 0, 0);
+    height: 60px;
+    border-radius: 10px;
+    padding: 10px;
+    background-image: linear-gradient(to left top, #65faff, #6df0ff, #83e5ff, #9cdaff, #b2ceff, #b0ceff, #aecfff, #accfff, #92dbff, #74e7ff, #5ef2ff, #5ffbf1);
+
+}
+
+.accuracy-text{
+    display: inline-block;
+    border: 0px solid red;
+    color: rgb(0, 0, 0);
+    font-size: 1.3rem;
+    font-weight: bold;
+    margin-right: 20px;
+    margin-left: 30px
 }
 
 .text-box-left{
