a64b0096600621259362fa43ee9cc84091e76aae
northcastle <975887168@qq.com>
2024-05-28 22:23:52 +0800

    try to add menu on my own

--- electron/electronMain.ts
+++ electron/electronMain.ts
@@ -2,9 +2,116 @@
  * electron 的主进程
  */
 // 导入模块
-import {app,BrowserWindow} from 'electron'
+import {app,BrowserWindow,Menu, MenuItemConstructorOptions} from 'electron'
 
 import path from 'path'
+
+// 是否为Mac的标识
+const isMac = process.platform === 'darwin'
+
+const template:Array<MenuItemConstructorOptions> = [
+  // { role: 'appMenu' }
+  // ...(isMac
+  //   ? [{
+  //       label: app.name,
+  //       submenu: [
+  //         { role: 'about' },
+  //         { type: 'separator' },
+  //         { role: 'services' },
+  //         { type: 'separator' },
+  //         { role: 'hide' },
+  //         { role: 'hideOthers' },
+  //         { role: 'unhide' },
+  //         { type: 'separator' },
+  //         { role: 'quit' }
+  //       ]
+  //     }]
+  //   : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  // {
+  //   label: 'Edit',
+  //   submenu: [
+  //     { role: 'undo' },
+  //     { role: 'redo' },
+  //     { type: 'separator' },
+  //     { role: 'cut' },
+  //     { role: 'copy' },
+  //     { role: 'paste' },
+  //     ...(isMac
+  //       ? [
+  //           { role: 'pasteAndMatchStyle' },
+  //           { role: 'delete' },
+  //           { role: 'selectAll' },
+  //           { type: 'separator' },
+  //           {
+  //             label: 'Speech',
+  //             submenu: [
+  //               { role: 'startSpeaking' },
+  //               { role: 'stopSpeaking' }
+  //             ]
+  //           }
+  //         ]
+  //       : [
+  //           { role: 'delete' },
+  //           { type: 'separator' },
+  //           { role: 'selectAll' }
+  //         ])
+  //   ]
+  // },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  // {
+  //   label: 'Window',
+  //   submenu: [
+  //     { role: 'minimize' },
+  //     { role: 'zoom' },
+  //     ...(isMac
+  //       ? [
+  //           { type: 'separator' },
+  //           { role: 'front' },
+  //           { type: 'separator' },
+  //           { role: 'window' }
+  //         ]
+  //       : [
+  //           { role: 'close' }
+  //         ])
+  //   ]
+  // },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://electronjs.org')
+        }
+      }
+    ]
+  }
+]
+
 
 /**
  * 创建主窗口 edited on 2024-05-27
@@ -12,11 +119,18 @@ import path from 'path'
  * @param heightValue 窗口高度
  */
 const createWindow = (widthValue:number,heightValue:number) => {
+  // 主窗口
   const win = new BrowserWindow({
     width: widthValue,
     height:heightValue,
     resizable:false, // 禁止改变窗口大小
   })
+
+  // 设置自定义的菜单
+  const menu : Menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+  console.log(menu)
+  console.log(' --> add menu end')
 
   // 根据是否存在开发路径，决定加载不同的内容
   let devUrl = process.argv[2]
@@ -32,7 +146,7 @@ const createWindow = (widthValue:number,heightValue:number) => {
 // 应用准备就绪，加载窗口
 app.whenReady().then(() => {
 
-  if (process.platform !== 'darwin'){
+  if (!isMac){
     // 非mac系统
     createWindow(1200,863)
   }else{
