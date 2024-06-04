a09feb5837d0253f2eec3ba2e44656775d5a728a
wanghongchang <975887168@qq.com>
2024-06-04 16:55:26 +0800

    finish the get checked nodes and show on right

--- src/views/packageHelper/PackageHelper.vue
+++ src/views/packageHelper/PackageHelper.vue
@@ -15,11 +15,12 @@
       <el-col :span="11" >
         <el-card class="card-container card-container-1">
           <template #header>
-            <div class="card-header-left">待选择的文件树</div>
+            <span class="card-header-left">待选择的文件树</span>
+            <el-button type="primary" size="small" :icon="DArrowRight"   style="margin-left: 55%;" @click="confirmChooseFiles"/>
           </template>
           <div class="el-tree-box">
             <el-scrollbar max-height="600px">
-              <el-tree  empty-text="暂无数据" :data="filesTreeData" :props="treeProps" 
+              <el-tree ref="filesTree" empty-text="暂无数据" :data="filesTreeData" :props="treeProps" 
                     node-key="id" show-checkbox  default-expand-all class="tree-class" >
                 
                 <!-- node : 当前的节点对象；data : 当前节点的数据 -->
@@ -42,12 +43,13 @@
       <el-col :span="11" :offset="2" >
         <el-card class="card-container card-container-2" style="text-align: right;">
           <template #header>
-            <div class="card-header-right">已选中的文件树</div>
+            <el-button type="warning" size="small" :icon="DArrowLeft"   style="margin-right: 55%;"/>
+            <span class="card-header-right">已选中的文件树</span>
           </template>
 
           <div class="el-tree-box">
             <el-scrollbar max-height="600px">
-              <el-tree  empty-text="暂未选中数据" :data="filesTreeData" :props="treeProps" 
+              <el-tree  empty-text="暂未选中数据" :data="filesTreeDataChoosed" :props="treeProps" 
                   node-key="id" show-checkbox  default-expand-all class="tree-class">
                 <!-- node : 当前的节点对象；data : 当前节点的数据 -->
                 <template #default="{ node, data }">
@@ -71,7 +73,12 @@
 
 <script setup lang="ts">
 
-  import { reactive } from 'vue';
+  import { ref,reactive } from 'vue';
+
+  import { ElTree } from 'element-plus'
+
+  import { DArrowRight,DArrowLeft } from '@element-plus/icons-vue'
+
 
   import folderIcon from '/folder.svg'
   import fileIcon from '/file.svg'
@@ -81,8 +88,10 @@
 
   // 树形组件的数据类型
   import type { TreeNode } from './PackageHelperType'
+import type { TreeNodeData } from 'element-plus/es/components/tree-v2/src/types.mjs';
 
-
+  // 左侧的待选择的树对象
+  const filesTree = ref<InstanceType<typeof ElTree>>()
 
   // 树形组件的配置
   const treeProps = {
@@ -110,6 +119,80 @@
       })
     }
     // console.log('filesTreeData :',filesTreeData)
+  }
+
+  /**
+   * 确定选中的文件
+   */
+  const confirmChooseFiles = () => {
+    if(filesTreeData.length > 0){
+      // 先清空一下原来的数据
+      filesTreeDataChoosed.splice(0,filesTreeDataChoosed.length)
+      // 获取选中的文件数组
+      let choosedFilesList:TreeNodeData[] = filesTree.value!.getCheckedNodes(false, true)
+      console.log('选中的文件列表 :',choosedFilesList)
+      // 遍历选中的文件列表
+      if(choosedFilesList && choosedFilesList.length > 0){
+        // 创建新的右侧的数据列表
+        let rootData =  choosedFilesList.splice(0,1)[0]
+        console.log('根目录数据 :',rootData)
+        console.log('剩余目录数据 :',choosedFilesList)
+
+        createTreeWhichChoosed(rootData,choosedFilesList,filesTreeDataChoosed)
+        console.log('创建后的树 :',filesTreeDataChoosed)
+
+      }
+    }
+  }
+
+  /**
+   * 创建右侧选中的新的树
+   * @param rootData 根节点
+   * @param choosedList 选中的节点
+   */
+  const createTreeWhichChoosed = (rootData:TreeNodeData,choosedList:TreeNodeData[],targetTreeNodeData:TreeNode[])=>{
+      if(rootData){
+
+        //console.log(rootData.fullPath + "    --begin--")
+        let nodeData:TreeNode = {
+          id: rootData.id, // 主键值
+          label: rootData.label, // 节点名称
+          parentPath:rootData.parentPath,// 父节点路径
+          fullPath: rootData.fullPath, // 节点路径
+          isDir: rootData.isDir, // 是否是文件夹
+          children: [], // 子节点集合
+        }
+
+        if(choosedList && choosedList.length > 0){ // 循环子节点进行递归创建
+          choosedList.forEach(element => {
+            if(element.parentPath == rootData.fullPath){ // 是直接子集的时候，才可以进行数据组装
+              if(element.isDir){ // 是目录的时候继续递归
+                //console.log('folder : ',element)
+                // 过滤掉当前的节点
+                const filterList = choosedList.filter(item => item.id !== element.id)
+                createTreeWhichChoosed(element,filterList,nodeData.children)
+              }else{ // 是文件的时候,直接放进去
+                  //console.log('file : ',element)
+                  if(element.parentPath == nodeData.fullPath){
+                    // 匹配成功，添加到children中
+                    nodeData.children.push({
+                      id: element.id, // 主键值
+                      label: element.label, // 节点名称
+                      parentPath:element.parentPath,// 父节点路径
+                      fullPath: element.fullPath, // 节点路径
+                      isDir: element.isDir, // 是否是文件夹
+                      children: [], // 子节点集合
+                    })
+                  }
+              }
+            }
+          });
+        }
+        // 最后把根节点放进来
+        targetTreeNodeData.push(nodeData);
+
+        //console.log(rootData.fullPath + "    --end--")
+      }
   }
   
 </script>
@@ -160,7 +243,7 @@
   background-color: rgb(97, 207, 33);
   color: aliceblue;
   font-weight: bold;
-  margin-right: 10px;
+  margin-right: 5px;
 }
 
 .el-tree-box{
