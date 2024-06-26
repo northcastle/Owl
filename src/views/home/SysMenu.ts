fff78758172b49726e91b12b5770d096ce99a3f0
northcastle <975887168@qq.com>
2024-06-26 20:27:01 +0800

    show the typingPractice

--- src/views/home/SysMenu.ts
+++ src/views/home/SysMenu.ts
@@ -6,7 +6,7 @@ export const defaultMenuRouter = '/welcome'
 // 声明一些菜单元素
 export const menuList:Array<MenuObj> = [
   {id:'a',text:'首页',icon:'Location',routerPath:'/welcome',isShow:true},
-  {id:'b',text:'打字练习',icon:'Menu',routerPath:'/typingPractice',isShow:false},
+  {id:'b',text:'打字练习',icon:'Menu',routerPath:'/typingPractice',isShow:true},
   {id:'c',text:'协助打包',icon:'Files',routerPath:'/packageHelper',isShow:true},
 
 ]
